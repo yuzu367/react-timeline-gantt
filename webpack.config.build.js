3824f6a049b9aab89241fa1c1e26e868826ebdbc
yuzu367 <popova.anna235@gmail.com>
2025-09-26 19:54:38 +0300

    webpack sizeme alias

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,53 +1,53 @@
-const path =require('path');
+const path = require('path');
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-module.exports ={
+module.exports = {
   //Where to read where to output
-  entry:'./src/lib/TimeLine.js',
-  output:{
-    path:path.resolve('dist'),
-    filename:'index.js',
+  entry: './src/lib/TimeLine.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js',
     libraryTarget: 'umd',
     library: 'react-gantt-timeline',
-    umdNamedDefine: true  
+    umdNamedDefine: true
   },
-  resolve: {      
+  resolve: {
     alias: {
-        'moment' : path.resolve(__dirname, './node_modules/moment'),         
-        'react': path.resolve(__dirname, './node_modules/react'),
-        'react-dom': path.resolve(__dirname, './node_modules/react-dom'),    
-        'react-sizeme': path.resolve(__dirname, './node_modules/react-sizeme'),     
-         libs: path.resolve(__dirname, 'src/lib/'),
-    }  
-  },  
-  externals: {      
-      // Don't bundle react or react-dom      
-      react: {          
-          commonjs: "react",          
-          commonjs2: "react",          
-          amd: "React",          
-          root: "React"      
-      },      
-      "react-dom": {          
-          commonjs: "react-dom",          
-          commonjs2: "react-dom",          
-          amd: "ReactDOM",          
-          root: "ReactDOM"      
-      } ,
-      "react-sizeme": {          
-        commonjs: "react-sizeme",          
-        commonjs2: "react-sizeme",          
-        amd: "ReactDOM",          
-        root: "ReactDOM"      
+      'moment': path.resolve(__dirname, './node_modules/moment'),
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+      'react-sizeme': path.resolve(__dirname, 'node_modules/react-sizeme'),
+      libs: path.resolve(__dirname, 'src/lib/'),
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom      
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
     },
-      moment: {          
-        commonjs: "moment",          
-        commonjs2: "moment",          
-        amd: "moment",          
-        root: "moment"      
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    },
+    "react-sizeme": {
+      commonjs: "react-sizeme",
+      commonjs2: "react-sizeme",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    },
+    moment: {
+      commonjs: "moment",
+      commonjs2: "moment",
+      amd: "moment",
+      root: "moment"
     },
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -59,7 +59,7 @@ module.exports ={
         test: /\.css$/,
         exclude: /node_modules/,
         use: ["style-loader", "css-loader"]
-       },
+      },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         exclude: /node_modules/,
