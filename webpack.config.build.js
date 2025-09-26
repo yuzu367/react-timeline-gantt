7bc28e74234d9e4e63538ff63ddffc908b36c1ac
yuzu367 <popova.anna235@gmail.com>
2025-09-26 20:34:47 +0300

    webpack modules

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -15,7 +15,7 @@ module.exports = {
       'moment': path.resolve(__dirname, './node_modules/moment'),
       'react': path.resolve(__dirname, './node_modules/react'),
       'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-      'react-sizeme': path.resolve(__dirname, './node_modules/react-sizeme'),
+      'react-sizeme': path.resolve(__dirname, '../../react-sizeme'),
       libs: path.resolve(__dirname, 'src/lib/'),
     },
     modules: [
